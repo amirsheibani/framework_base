f4c8ab7d764c03a10fe6446904b875d5fd6a93d7
a_sheibani <a_sheibani@keywa.ca>
2026-02-14 20:09:40 +0330

    add framework_base

--- lib/packages/framework_utils/lib/utils_framework.dart
+++ lib/packages/framework_utils/lib/utils_framework.dart
@@ -6,5 +6,7 @@ export 'src/extensions/int_extension.dart';
 export 'src/extensions/jalali_extension.dart';
 export 'src/extensions/search_debounce_extension.dart';
 export 'src/extensions/date_time_extension.dart';
+export 'src/interceptor/custom_pretty_logger.dart';
+
 
 
