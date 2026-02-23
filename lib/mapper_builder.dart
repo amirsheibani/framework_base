1944b33c2cb19f50c8041f7379a726806fa68122
a_sheibani <a_sheibani@keywa.ca>
2026-02-23 16:29:23 +0330

    implement storage_framework

--- lib/mapper_builder.dart
+++ lib/mapper_builder.dart
@@ -1,4 +1,4 @@
 import 'package:build/build.dart';
-import 'package:framework_base/packages/dart_mapper/lib/builder.dart' as impl;
+import 'package:framework_base/packages/dart_mapper/builder.dart' as impl;
 
 Builder mapperBuilder(BuilderOptions options) => impl.mapperBuilder(options);
