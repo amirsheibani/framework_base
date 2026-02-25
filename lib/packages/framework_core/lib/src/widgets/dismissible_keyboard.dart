076b676962beefe1e6878277fb92e334311ecbf3
a_sheibani <a_sheibani@keywa.ca>
2026-02-25 09:14:37 +0330

    fix DismissibleKeyboard bug on ios

--- lib/packages/framework_core/lib/src/widgets/dismissible_keyboard.dart
+++ lib/packages/framework_core/lib/src/widgets/dismissible_keyboard.dart
@@ -11,10 +11,10 @@ class DismissibleKeyboard extends StatelessWidget {
       behavior: HitTestBehavior.translucent,
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
-        // final FocusScopeNode currentFocus = FocusScope.of(context);
-        // if (currentFocus.focusedChild != null) {
-        //   FocusManager.instance.primaryFocus!.unfocus();
-        // }
+        final FocusScopeNode currentFocus = FocusScope.of(context);
+        if (currentFocus.focusedChild != null) {
+          FocusManager.instance.primaryFocus!.unfocus();
+        }
       },
       child: child,
     );
